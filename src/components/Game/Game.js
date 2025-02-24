130bb2e1018d9c6a7cbf02c154bc8516ef81babe
Danny <46854315+DanFCo@users.noreply.github.com>
2025-02-23 21:04:43 -0500

    fix stale guesses count in state

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -22,14 +22,21 @@ function Game() {
 
   function handleSubmitGuess(guess) {
     const checkedGuess = checkGuess(guess, answer);
-    setGuesses((prevGuesses) => [...prevGuesses, checkedGuess]);
+    const nextGuesses = [...guesses, checkedGuess] // this is to fix the stale array for checking length
+    // setGuesses((prevGuesses) => [...prevGuesses, checkedGuess]);
+    setGuesses(nextGuesses)
 
     if(guess === answer){
       setShowHappyBanner(true)
     }
 
-    if(guesses.length + 1 === NUM_OF_GUESSES_ALLOWED){
-      setShowSadBanner(true)
+    // kind of hacky and only because the guesses array in state is stale and doesn't update until the next render.
+    // if(guesses.length + 1 === NUM_OF_GUESSES_ALLOWED){
+    //   setShowSadBanner(true)
+    // }
+
+    if(nextGuesses.length === NUM_OF_GUESSES_ALLOWED){
+      setShowSadBanner(true);
     }
   }
 
